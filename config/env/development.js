44c5968ab52317fdf8b684ea9aa9380fdf8976cc
DOUGLAS LIMA DIAS <douglas.dias@centralit.com.br>
2019-10-01 12:57:09 -0300

    update

---
+++ config/env/development.js
@@ -0,0 +1,31 @@
+/**
+ * Development environment settings
+ *
+ * This file can include shared settings for a development team,
+ * such as API keys or remote database passwords.  If you're using
+ * a version control solution for your Sails app, this file will
+ * be committed to your repository unless you add it to your .gitignore
+ * file.  If your repository will be publicly viewable, don't add
+ * any private information to this file!
+ *
+ */
+
+module.exports = {
+
+  hookTimeout               : 60000,
+
+  VERSION                   : 'v2',
+  VERSION_DATE              : "2018-09-20",
+
+  CONVERSATION_USERNAME     : "b41326bd-8e10-436f-9656-6fd8c349fef2",
+	CONVERSATION_PASSWORD     : "exzZi1p3OlsA",
+	CONVERSATION_URL          : "https://gateway.watsonplatform.net/conversation/api",
+
+  CITSMART_USERNAME         : "",
+  CITSMART_PASSWORD         : "",
+	CITSMART_URL              : "",
+  CITSMART_NUMBER_SERVICE   : "3570",
+  CITSMART_GET_NAMESERV_BYID: "GET_NAMESERV_BYID",
+
+
+};
