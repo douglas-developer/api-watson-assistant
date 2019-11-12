a15ea5869241b7460daa6af23daaae1b251a2aea
Douglas Lima Dias <douglaslimaa.dias@gmail.com>
2019-11-12 15:29:57 -0300

    Update development.js
    Alteração nos usuários

--- config/env/development.js
+++ config/env/development.js
@@ -17,15 +17,15 @@ module.exports = {
   VERSION                   : 'v2',
   VERSION_DATE              : "2018-09-20",
 
-  CONVERSATION_USERNAME     : "b41326bd-8e10-436f-9656-6fd8c349fef2",
-	CONVERSATION_PASSWORD     : "exzZi1p3OlsA",
-	CONVERSATION_URL          : "https://gateway.watsonplatform.net/conversation/api",
+  CONVERSATION_USERNAME     : "",
+	CONVERSATION_PASSWORD     : "",
+	CONVERSATION_URL          : "",
 
   CITSMART_USERNAME         : "",
   CITSMART_PASSWORD         : "",
 	CITSMART_URL              : "",
-  CITSMART_NUMBER_SERVICE   : "3570",
-  CITSMART_GET_NAMESERV_BYID: "GET_NAMESERV_BYID",
+  CITSMART_NUMBER_SERVICE   : "",
+  CITSMART_GET_NAMESERV_BYID: "",
 
 
 };
